110186f7dc192e79f9ea849e13845e35f7e177a5
Dave Murphy <davem@devkitpro.org>
2021-06-09 22:45:03 +0100

    freeimage init/deinit

--- src/nds_texcompress/nds_texcompress.cpp
+++ src/nds_texcompress/nds_texcompress.cpp
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include <FreeImage.h>
 #include "compress_texture.h"
 
 int main(int argc, char* argv[])
 {
+	int exitcode = 0;
+
+	FreeImage_DeInitialise();
+
 	for (int i = 1; i < argc; ++i)
 	{
 		printf("compressing %s...\n", argv[i]);
-		if (!compressTexture(argv[i])) return -1;
+		if (!compressTexture(argv[i]))
+		{
+			exitcode = -1;
+			break;
+		}
 	}
-	
-	return 0;
+
+	FreeImage_DeInitialise();
+
+	return exitcode;
 }
